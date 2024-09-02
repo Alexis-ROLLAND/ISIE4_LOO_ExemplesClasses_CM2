bb67a5a5043a7178001bf3a9e66b721c03cb07eb
Alexis ROLLAND <alexis.rolland@univ-tours.fr>
2024-09-02 16:54:21 +0200

    last changes

--- include/Led.hpp
+++ include/Led.hpp
@@ -6,35 +6,12 @@
 #include <string_view>
 
 class Led{
-    public:
-        enum class Couleur_t:uint8_t {  RED, 
-                                        ORANGE,
-                                        YELLOW,
-                                        GREEN,
-                                        BLUE,
-                                        WHITE 
-        };
-
-        enum class Etat_t:uint8_t{OFF, ON};
-                
+                   
     private:
-        Couleur_t   Color{Couleur_t::RED};
-        Etat_t      State{Etat_t::OFF};
-
-        void        setState(Etat_t Etat) noexcept {this->State = Etat;};
+        
 
     public:
     
-        [[nodiscard]]   Couleur_t   getColor() const noexcept   {return this->Color;};
-        void    setColor(Couleur_t Color) noexcept {this->Color = Color;};
-
-        [[nodiscard]]   Etat_t getState() const noexcept {return this->State;};
-
-        void    setON() noexcept {this->setState(Etat_t::ON);};
-        void    setOFF() noexcept {this->setState(Etat_t::OFF);};
-        void    toggle() noexcept;
-
-        [[nodiscard]]   std::string_view    to_string() const noexcept;
     
 };  /* Led  */
 
