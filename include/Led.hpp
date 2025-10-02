5051fb79be5aa40f26b793fd407832bfa25454ba
Alexis ROLLAND <alexis.rolland@univ-tours.fr>
2025-10-02 08:34:21 +0200

    End of work - 2025/2026

--- include/Led.hpp
+++ include/Led.hpp
@@ -1,22 +1,34 @@
 #ifndef __LED_HPP__
 #define __LED_HPP__
 
-#include <print>
 #include <cstdint>
+#include <print>
 #include <string_view>
 /**
  * @brief : Led Class example
  */
-class Led{
-                   
-    private:
-        
+class Led {
+  public:
+    enum class Couleur_t { RED, YELLOW, GREEN, BLUE };
+    enum class Etat_t { ON, OFF };
 
-    public:
-    
-    
-};  /* Led  */
+  private:
+    const Couleur_t _Couleur{Couleur_t::GREEN};
+    Etat_t _Etat{Etat_t::OFF};
 
+  public:
+    Led() = default;
+    Led(Couleur_t Couleur) : _Couleur{Couleur}, _Etat{Etat_t::OFF} {};
+    Led(Couleur_t Couleur, Etat_t Etat) : _Couleur{Couleur}, _Etat{Etat} {};
+    virtual ~Led() = default;
 
-#endif  /*  __LED_HPP__ */
+    [[nodiscard]] Couleur_t getCouleur() const noexcept { return this->_Couleur; };
+    [[nodiscard]] Etat_t getEtat() const noexcept { return this->_Etat; };
+    void setEtat(Etat_t Etat) noexcept { this->_Etat = Etat; };
 
+    void setOn() noexcept { this->setEtat(Etat_t::ON); };
+    void setOff() noexcept { this->setEtat(Etat_t::OFF); };
+    void toggle() noexcept;
+}; /* Led  */
+
+#endif /*  __LED_HPP__ */
--- src/test_Led.cpp
+++ src/test_Led.cpp
@@ -2,12 +2,16 @@
 
 #include <print>
 
-int main(){
+int main() {
 
     std::println("/- Test Led -\\");
 
-    
+    Led MaLedRouge{Led::Couleur_t::RED};
+    Led MaLed = MaLedRouge;
+
+    const Led MaLedJauneToujoursAllumee{Led::Couleur_t::YELLOW, Led::Etat_t::ON};
+
+    std::println("Couleur : {}", static_cast<int>(MaLed.getCouleur()));
+
     return 0;
 }
-
-
